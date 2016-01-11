b184d1b222bf14814a365414cdc204c61d5cff25
petrakiko <kelseyamills@gmail.com>
2016-01-11 16:41:00 -0500

    Create readTwoSensors.ino
    Code for testing ultrasonic sensor readings.

---
+++ prototype/readTwoSensors.ino
@@ -0,0 +1,26 @@
+//author: Sam Fiero
+
+String p0 = "Pin 0: ";
+String p1 = "Pin 1: ";
+String out = "Object Detected\n";
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(3, HIGH);
+  delay(1);
+  digitalWrite(3, LOW);
+  int sensor0 = (analogRead(A0)*4.9)/9.8;
+  int sensor1 = (analogRead(A1)*4.9)/9.8;
+  //if(sensor0 <= 80.00){
+   // Serial.println(out +  sensor0);
+  //}
+ Serial.println(p0 + sensor0);
+ Serial.println(p1 + sensor1);
+ 
+ delay(500);  
+}
